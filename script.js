6bb547e0ed221a2bf7bca36a6dfb87a89d27cb4a
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2024-01-12 22:05:28 +0500

    header js added

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
     logoText.classList.toggle('active');
 })
+
+let prevScrollPos = window.pageYOffset;
+const navbar = document.querySelector('.header');
+
+window.onscroll = () => {
+    const currentScrollPos = window.pageYOffset;
+
+    if (prevScrollPos > currentScrollPos) {
+        navbar.style.top = '0';
+    } else {
+        navbar.style.top = `-${navbar.offsetHeight}px`;
+    }
+
+    prevScrollPos = currentScrollPos;
+};
+
